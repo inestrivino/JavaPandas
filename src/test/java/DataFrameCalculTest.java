136ce2de79796dff6477d0a269308d7746dbddf4
Aazazel <eloise.blot@etu.univ-grenoble-alpes.fr>
2025-04-16 13:43:35 +0200

    ajout de cumsum, cumprod, min, max et des tests associés

--- src/test/java/DataFrameCalculTest.java
+++ src/test/java/DataFrameCalculTest.java
@@ -1,5 +1,11 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+
 import org.JavaPandas.DataFrame;
 import org.junit.Test;
 
@@ -14,6 +20,8 @@ public class DataFrameCalculTest {
         return df;
     }
 
+    //sum
+
     @Test
     public void testSumIntColumn() {
         DataFrame df = creer_DataFrame();
@@ -26,14 +34,14 @@ public class DataFrameCalculTest {
         assertEquals("la somme de 1.5, 2.5, 3.5 doit valoir 7.5",7.5, df.sum("col1"),0);
     }
 
-    //la somme d'une colonne d'un type non numérique doit renvoyer une IllegalArgumentException
+    //appeler sum sur une colonne d'un type non numérique renvoyer une IllegalArgumentException
     @Test(expected = IllegalArgumentException.class)
     public void testSumStringColumnThrowsException() {
         DataFrame df = creer_DataFrame();
         df.sum("col2");
     }
 
-    //la somme d'une colonne inexistante doit renvoyer une IllegalArgumentException
+    //appeler sum sur une colonne qui n'existe pas doit renvoyer une IllegalArgumentException
     @Test(expected = IllegalArgumentException.class)
     public void testSumUnknownColumnThrowsException() {
         DataFrame df = creer_DataFrame();
@@ -79,6 +87,12 @@ public class DataFrameCalculTest {
         assertEquals("la somme de 1000000, 2000000, 3000000 doit valoir 6000000",6000000,df.sum("col0"),0);
     }
 
+    //une RuntimeException doit être levée lorsqu'un constructeur de DataFrame est appelé avec un fichier qui n'existe pas
+    @Test(expected = RuntimeException.class)
+    public void testIOExceptionWhenFileDoesNotExist() {
+        new DataFrame("fichier_inexistant.csv");
+    }
+
     //mean
 
     @Test
@@ -95,13 +109,14 @@ public class DataFrameCalculTest {
         assertEquals("la moyenne de 1.5, 2.5, 3.5 doit valoir 2.5",2.5, mean, 0.0001);
     }
 
-    //la moyenne d'une colonne d'un type non numérique doit renvoyer une IllegalArgumentException
+    //appeler mean sur une colonne d'un type non numérique doit renvoyer une IllegalArgumentException
     @Test(expected = IllegalArgumentException.class)
     public void testMeanOnStringColumnThrowsException() {
         DataFrame df = creer_DataFrame();
         df.mean("col2");
     }
 
+    //appeler mean sur une colonne qui n'existe pas doit renvoyer une IllegalArgumentException
     @Test(expected = IllegalArgumentException.class)
     public void testMeanOnUnknownColumnThrowsException() {
         DataFrame df = creer_DataFrame();
@@ -139,21 +154,234 @@ public class DataFrameCalculTest {
         assertEquals("la moyenne de 1000000, 2000000, 3000000 doit valoir 2000000.0",2000000.0, df.mean("col0"), 0.0001);
     }
 
+    //cumsum
+
     @Test
-    public void testQuantile() {
-        DataFrame df = new DataFrame(new String[]{"int"});
-        //assertThrows(UnsupportedOperationException.class, () -> df.quantile("label"));
+    public void testCumsumIntColumn() {
+        DataFrame df = creer_DataFrame();
+        List<Object> expected = Arrays.asList(1.0, 3.0, 6.0);
+        assertEquals("cumsum sur une colonne contenant 1.0, 3.0, 6.0 doit renvoyer un Dataframe dont la colonne contient 1.0, 3.0, 6.0",expected, df.cumsum("col0").getData().get("col0"));
     }
 
     @Test
-    public void testCumsum() {
-        DataFrame df = new DataFrame(new String[]{"int"});
-        //assertThrows(UnsupportedOperationException.class, () -> df.cumsum("label"));
+    public void testCumsumDoubleColumn() {
+        DataFrame df = creer_DataFrame();
+        List<Object> expected = Arrays.asList(1.5, 4.0, 7.5);
+        assertEquals("cumsum sur une colonne contenant 1.5, 2.5, 3.5 doit renvoyer un Dataframe dont la colonne contient 1.5, 4.0, 7.5",expected, df.cumsum("col1").getData().get("col1"));
     }
 
     @Test
-    public void testCumprod() {
-        DataFrame df = new DataFrame(new String[]{"int"});
-        //assertThrows(UnsupportedOperationException.class, () -> df.cumprod("label"));
+    public void testCumsumNegativeAndPositive() {
+        DataFrame df = creer_DataFrame();
+        df.getData().get("col0").clear();
+        df.getData().get("col0").addAll(Arrays.asList(-5.0, -10.0, 15.0));
+        List<Object> expected = Arrays.asList(-5.0, -15.0, 0.0);
+        assertEquals("cumsum sur une colonne contenant -5.0, -10.0, 15.0 doit renvoyer un Dataframe dont la colonne contient -5.0, -15.0, 0.0",expected, df.cumsum("col0").getData().get("col0"));
     }
+
+    @Test
+    public void testCumsumNegative() {
+        DataFrame df = creer_DataFrame();
+        df.getData().get("col0").clear();
+        df.getData().get("col0").addAll(Arrays.asList(-5.0, -10.0, -15.0));
+        List<Object> expected = Arrays.asList(-5.0, -15.0, -30.0);
+        assertEquals("cumsum sur une colonne contenant -5.0, -10.0, -15.0 doit renvoyer un Dataframe dont la colonne contient -5.0, -15.0, -30.0",expected, df.cumsum("col0").getData().get("col0"));
+    }
+
+    //appeler cumsum sur une colonne d'un type non numérique doit renvoyer une IllegalArgumentException
+    @Test(expected = IllegalArgumentException.class)
+    public void testCumsumStringColumnThrowsException() {
+        DataFrame df = creer_DataFrame();
+        df.cumsum("col2");
+    }
+
+    //appeler cumsum sur une colonne qui n'existe pas doit renvoyer une IllegalArgumentException
+    @Test(expected = IllegalArgumentException.class)
+    public void testCumsumOnNonExistentColumnThrowsException() {
+        DataFrame df = creer_DataFrame();
+        df.cumsum("unknownCol");
+    }
+
+    @Test
+    public void testCumsumEmptyColumn() {
+        DataFrame emptyDf = new DataFrame(new String[] {"int"});
+        DataFrame result = emptyDf.cumsum("col0");
+        assertTrue(result.getData().get("col0").isEmpty());
+    }
+
+    @Test
+    public void testCumsumEmptyColumn2() {
+        DataFrame emptyDf = new DataFrame(new String[] {"int"});
+        List<Object> expected = Arrays.asList();
+        assertEquals("cumsum sur une colonne vide doit renvoyer un Dataframe dont la colonne est vide",expected, emptyDf.cumsum("col0").getData().get("col0"));
+    }
+
+    //cumprod
+
+    @Test
+    public void testCumprodIntColumn() {
+        DataFrame df = creer_DataFrame();
+        List<Object> expected = Arrays.asList(1.0, 2.0, 6.0);
+        assertEquals("cumprod sur une colonne contenant 1.0, 3.0, 6.0 doit renvoyer un Dataframe dont la colonne contient 1.0, 3.0, 6.0",expected, df.cumprod("col0").getData().get("col0"));
+    }
+
+    @Test
+    public void testCumprodIntZeroColumn() {
+        DataFrame df = creer_DataFrame();
+        df.getData().get("col0").clear();
+        df.getData().get("col0").addAll(Arrays.asList(1.0, 2.0, 0.0,17.0));
+        List<Object> expected = Arrays.asList(1.0, 2.0, 0.0,0.0);
+        assertEquals("cumprod sur une colonne contenant 1.0, 2.0, 0.0, 17.0 doit renvoyer un Dataframe dont la colonne contient 1.0, 2.0, 0.0, 0.0",expected, df.cumprod("col0").getData().get("col0"));
+    }
+
+    @Test
+    public void testCumprodDoubleColumn() {
+        DataFrame df = creer_DataFrame();
+        List<Object> expected = Arrays.asList(1.5, 3.75, 13.125);
+        assertEquals("cumprod sur une colonne contenant 1.5, 2.5, 3.5 doit renvoyer un Dataframe dont la colonne contient 1.5, 3.75, 5.625",expected, df.cumprod("col1").getData().get("col1"));
+    }
+
+    @Test
+    public void testCumprodDoubleZeroColumn() {
+        DataFrame df = creer_DataFrame();
+        df.getData().get("col0").clear();
+        df.getData().get("col0").addAll(Arrays.asList(1.5, 2.5, 0.0, 17.5));
+        List<Object> expected = Arrays.asList(1.5, 3.75, 0.0,0.0);
+        assertEquals("cumprod sur une colonne contenant 1.5, 2.5, 0.0, 17.5 doit renvoyer un Dataframe dont la colonne contient 1.5, 3.75, 0.0,0.0",expected, df.cumprod("col0").getData().get("col0"));
+    }
+
+    @Test
+    public void testCumprodNegativeAndPositive() {
+        DataFrame df = creer_DataFrame();
+        df.getData().get("col0").clear();
+        df.getData().get("col0").addAll(Arrays.asList(-5.0, -10.0, 15.0));
+        List<Object> expected = Arrays.asList(-5.0, 50.0, 750.0);
+        assertEquals("cumprod sur une colonne contenant -5.0, -10.0, 15.0 doit renvoyer un Dataframe dont la colonne contient -5.0, 50.0, 750.0",expected, df.cumprod("col0").getData().get("col0"));
+    }
+
+    @Test
+    public void testCumprodNegative() {
+        DataFrame df = creer_DataFrame();
+        df.getData().get("col0").clear();
+        df.getData().get("col0").addAll(Arrays.asList(-5.0, -10.0, -15.0));
+        List<Object> expected = Arrays.asList(-5.0, 50.0, -750.0);
+        assertEquals("cumprod sur une colonne contenant -5.0, -10.0, -15.0 doit renvoyer un Dataframe dont la colonne contient -5.0, 50.0, -750.0",expected, df.cumprod("col0").getData().get("col0"));
+    }
+
+    //appeler cumprod sur une colonne d'un type non numérique doit renvoyer une IllegalArgumentException
+    @Test(expected = IllegalArgumentException.class)
+    public void testCumprodStringColumnThrowsException() {
+        DataFrame df = creer_DataFrame();
+        df.cumprod("col2");
+    }
+
+    //appeler cumprod sur une colonne qui n'existe pas doit renvoyer une IllegalArgumentException
+    @Test(expected = IllegalArgumentException.class)
+    public void testCumprodOnNonExistentColumnThrowsException() {
+        DataFrame df = creer_DataFrame();
+        df.cumprod("unknownCol");
+    }
+
+    @Test
+    public void testCumprodEmptyColumn() {
+        DataFrame emptyDf = new DataFrame(new String[] {"int"});
+        DataFrame result = emptyDf.cumprod("col0");
+        assertTrue(result.getData().get("col0").isEmpty());
+    }
+
+    @Test
+    public void testCumprodEmptyColumn2() {
+        DataFrame emptyDf = new DataFrame(new String[] {"int"});
+        List<Object> expected = Arrays.asList();
+        assertEquals("cumprod sur une colonne vide doit renvoyer un Dataframe dont la colonne est vide",expected, emptyDf.cumprod("col0").getData().get("col0"));
+    }
+
+    //max
+
+    @Test
+    public void testMax1(){
+        DataFrame df = creer_DataFrame();
+        assertEquals("le max de 1 2 3 est 3",3, df.max("col0"),0);
+    }
+
+    @Test
+    public void testMax2(){
+        DataFrame df = creer_DataFrame();
+        assertEquals("le max de 1.5, 2.5, 3.5 est 3.5",3.5, df.max("col1"),0);
+    }
+
+    @Test
+    public void testMax3(){
+        DataFrame df = creer_DataFrame();
+        df.getData().get("col0").clear();
+        df.getData().get("col0").addAll(Arrays.asList(-5.0, -10.0, -15.0));
+        assertEquals("le max de -5.0, -10.0, -15.0",-5.0, df.max("col0"),0);
+    }
+
+    @Test
+    public void testMax4(){
+        DataFrame df = creer_DataFrame();
+        df.getData().get("col0").clear();
+        df.getData().get("col0").addAll(Arrays.asList(-5.0, -10.0, 15.0));
+        assertEquals("le max de -5.0, -10.0, 15.0",15.0, df.max("col0"),0);
+    }
+
+    //appeler max sur une colonne d'un type non numérique doit renvoyer une IllegalArgumentException
+    @Test(expected = IllegalArgumentException.class)
+    public void testMax5() {
+        DataFrame df = creer_DataFrame();
+        df.max("col2");
+    }
+
+    //appeler max sur une colonne qui n'existe pas doit renvoyer une IllegalArgumentException
+    @Test(expected = IllegalArgumentException.class)
+    public void testMax6() {
+        DataFrame df = creer_DataFrame();
+        df.max("inexistant");
+    }
+
+    //min
+
+    @Test
+    public void testMin1(){
+        DataFrame df = creer_DataFrame();
+        assertEquals("le min de 1, 2, 3 est 1",1, df.min("col0"),0);
+    }
+
+    @Test
+    public void testMin2(){
+        DataFrame df = creer_DataFrame();
+        assertEquals("le min de 1.5, 2.5, 3.5 est 3.5 est 1.5",1.5, df.min("col1"),0);
+    }
+
+    @Test
+    public void testMin3(){
+        DataFrame df = creer_DataFrame();
+        df.getData().get("col0").clear();
+        df.getData().get("col0").addAll(Arrays.asList(-5.0, -10.0, -15.0));
+        assertEquals("le min de -5.0, -10.0, -15.0 est -15.0",-15.0, df.min("col0"),0);
+    }
+
+    @Test
+    public void testMin4(){
+        DataFrame df = creer_DataFrame();
+        df.getData().get("col0").clear();
+        df.getData().get("col0").addAll(Arrays.asList(-5.0, -10.0, 15.0));
+        assertEquals("le min de -5.0, -10.0, 15.0 est -10.0",-10.0, df.min("col0"),0);
+    }
+
+    //appeler min sur une colonne d'un type non numérique doit renvoyer une IllegalArgumentException
+    @Test(expected = IllegalArgumentException.class)
+    public void testMin5() {
+        DataFrame df = creer_DataFrame();
+        df.min("col2");
+    }
+
+    //appeler min sur une colonne qui n'existe pas doit renvoyer une IllegalArgumentException
+    @Test(expected = IllegalArgumentException.class)
+    public void testMin6() {
+        DataFrame df = creer_DataFrame();
+        df.min("inexistant");
+    }
+
 }
